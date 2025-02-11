03411508cb3e86a5c8e09fd064ae52cfb8d9d7e1
DanielTakeyama <daniel.martins.a.prado@gmail.com>
2025-02-11 16:09:29 -0300

    feat: scenary-manager.js criado com sucesso, esse Manager é responsável pelo background do jogo

--- src/game.js
+++ src/game.js
@@ -1,4 +1,5 @@
 import { PlayerManager } from './managers/player-manager.js';
+import { ScenaryManager } from './managers/scenery-manager.js';
 
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
@@ -11,9 +12,12 @@ function initialize(ctx, canvas){
     ctx.clearRect(0,0, canvas.width, canvas.height);
 
     const player = new PlayerManager();
+    const cenario = new ScenaryManager();
+    
+    cenario.draw(ctx, canvas.width, canvas.height);
     player.spawn(ctx);
 
-
+    
     requestAnimationFrame(()=> initialize(ctx, canvas));
 }
 
--- src/managers/scenery.js
+++
@@ -1,7 +0,0 @@
-import { ImageComponent } from "../ecm-components/image-component";
-
-export class ScenaryManager{
-    constructor(){
-        this.background = new ImageComponent("../../assets/background.jpg");
-    }
-}
